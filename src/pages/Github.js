eb5a11f4accd4c86806cadc96ed7305171fdfe8c
Kristian Tjelta Johansen <kritjo@kritjo.com>
2022-06-14 20:44:38 +0200

    Addded card carousel with data from github API

--- src/pages/Github.js
+++ src/pages/Github.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import GhCarousel from "../components/carousel";
+
 const Github = () => {
+    const [loading, setLoading] = useState(false);
+    const [repos, setRepos] = useState([])
+
+    useEffect(() => {
+        const loadRepo = async () => {
+            setLoading(true);
+
+            const response = await axios.get(
+                "https://api.github.com/users/kritjo/subscriptions"
+            );
+
+            setRepos(response.data)
+            setLoading(false);
+        }
+
+        loadRepo();
+    }, []);
+
     return (
         <div>
-            <a className="App-link" href={"https://github.com/kritjo"}>Github</a>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <GhCarousel repos={repos} />
+            )
+            }
         </div>
     );
 }
